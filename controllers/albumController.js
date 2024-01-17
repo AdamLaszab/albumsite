aeaa212f7a7796e67801d51375f38e87cf04d859
Adam Laszab <adam.laszab@gmail.com>
2024-01-17 16:33:15 +0100

    added summary into albums

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -5,7 +5,6 @@ const Genre = require("../models/genre");
 const validator = require("express-validator");
 const body = validator.body;
 const validationResult = validator.validationResult;
-const summary= "PLACEHOLDER";
 exports.album_list = asyncHandler(async(req,res,next)=>{
 const allAlbums = await Album.find({},"title artist summary genres").sort({title:1}).populate("artist genres").exec(); 
 res.render("album_list",{title:"Album list",album_list:allAlbums});
@@ -19,14 +18,15 @@ exports.album_add_get= asyncHandler(async(req,res,next)=>{
     const AllArtists = await Artist.find({},"name").sort({name:1}).exec();
     res.render("album_form",{title:"Album form",errors:undefined,genres:AllGenres,artists:AllArtists});
 })
-exports.album_add_post=[body("name","genre name has to be longer than 1 characters").trim().isLength({min:1}).escape()
+exports.album_add_post=[body("name","genre name has to be longer than 1 characters").trim().isLength({min:1}).escape(),
+    body("albumSummary","Summary cant be empty").trim().isLength({min:1}).escape()
  ,asyncHandler(async(req,res,next)=>{
     const errors = validationResult(req);
-    const newAlbum = new Album({title:req.body.name,summary:summary,artist:req.body.artist,genres:[req.body.genre]});
+    const newAlbum = new Album({title:req.body.name,summary:req.body.albumSummary,artist:req.body.artist,genres:[req.body.genre]});
     if(!errors.isEmpty()){
         res.render("album_form",{title:"Album form",errors:errors.array()})
     }else{
-        const albumExists = await Album.findOne({title:req.body.name,summary:summary,artist:req.body.artist,genres:[req.body.genre]}).collation({locale:"en",strength:2}).exec();
+        const albumExists = await Album.findOne({title:req.body.name,summary:req.body.albumSummary,artist:req.body.artist,genres:[req.body.genre]}).collation({locale:"en",strength:2}).exec();
         if(albumExists){
             res.redirect(albumExists.url);
         }else{
