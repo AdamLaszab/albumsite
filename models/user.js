c275973ff30d5d923aa110e4fc5f8339a15052c5
Adam Laszab <adam.laszab@gmail.com>
2024-02-22 23:01:04 +0100

    Finishing touches

--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const RatingSchema = new Schema({
+  entity: { type: Schema.Types.ObjectId, required: true }, 
+  rating: { type: Number, required: true },
+});
+
 const UserSchema = new Schema({
     username: {type:String, required:true},
     password: {type:String, required:true},
-    admin: {type:Boolean,required:true}
+    admin: {type:Boolean,required:true},
+    ratings:[RatingSchema]
 })
 
 module.exports = mongoose.model("User",UserSchema);
